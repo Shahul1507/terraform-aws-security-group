6ffb38e428774b5c906ba8d5e207a32b4691f501
Shahul1507 <shahul.met@gmail.com>
2024-10-09 14:21:30 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+#sg_name = expense-env-mysql
+
+variable "project_name" {
+
+}
+
+variable "environment" {
+ 
+}
+
+variable "sg_name" {
+
+}
+
+variable "common_tags" {
+    default={}
+}
+
+variable "sg_tags"{
+    default = {}
+}
+
+variable "vpc_id" {
+}
